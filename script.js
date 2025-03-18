b34079285ffe90f541b69c10896382be8f49c3e5
Hasan Rahman <hasanrahman33605@gmail.com>
2025-03-18 14:21:40 +0600

    save functionality complete

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const moodDate = document.querySelector("#mood-date");
   const moodBtn = document.querySelectorAll(".mood-btn");
   const saveBtn = document.querySelector("#save-mood");
-  const moodDate = document.querySelector("#mood-history");
-  
+
+  let selectedMood = null;
+
+  //select Mood Emoji
+  moodBtn.forEach((btn) => {
+    btn.addEventListener("click", function () {
+      selectedMood = this.getAttribute("data-mood");
+    });
+  });
+
+  //save Data
+  saveBtn.addEventListener("click", function () {
+    //validation
+    if (!selectedMood || !moodDate) {
+      alert("Please select a both Mood Emoji and Date");
+      return;
+    }
+
+    const selectedDate = moodDate.value;
+
+    let moods = JSON.parse(localStorage.getItem("moods")) || {};
+
+    moods[selectedDate] = selectedMood;
+    //save in local Storage
+    localStorage.setItem("moods", JSON.stringify(moods));
+  });
 });
